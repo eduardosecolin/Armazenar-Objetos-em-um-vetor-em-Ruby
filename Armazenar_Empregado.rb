279b2c1fa5a11d67f006542f2704460f58b3746c
Eduardo Secolin <dusecolin@yahoo.com.br>
2017-12-02 15:40:47 -0200

    Instanciando um Empregado

--- Armazenar_Empregado.rb
+++ Armazenar_Empregado.rb
@@ -1,21 +1,56 @@
 # CLASSE EMPREGADO
 class Empregado
- attr_reader :codigo, :nome, :setor, :salarioBruto, :inss
+ attr_accessor :codigo, :nome, :setor, :salarioBruto, :inss
 
- def Empregado(codigo, nome, setor, salario)
-  @codigo = codigo
-  @nome = nome
-  @setor = setor
-  @salarioBruto = salario
-  @inss = 0.0
+ def Empregado()
  end
 
  def calcularInss
+  if (@salarioBruto <= ParametrosInss.FAIXA[0])
+    @inss = @salarioBruto * 0.08
+  elsif ((@salarioBruto < ParametrosInss.FAIXALIMITE[0]) && (@salarioBruto <= ParametrosInss.FAIXA[1]))
+    @inss = @salarioBruto * 0.09
+  elsif ((@salarioBruto < ParametrosInss.FAIXALIMITE[1]) && (@salarioBruto <= ParametrosInss.FAIXA[2]))
+    @inss = @salarioBruto * 0.11
+  else
+    @inss = ParametrosInss.TETO * 0.11
+  end
  end
 end
 # CLASSE PARAMETROS INSS
 class ParametrosInss
- self.attr_reader :FAIXA = [1659.38, 2765.66, 5531.31]
- self.attr_reader :FAIXALIMITE = [1659.39, 2765.67, 5531.32]
- self.attr_reader :TETO = 4663.75
+ @FAIXA = [1659.38, 2765.66, 5531.31]
+ @FAIXALIMITE = [1659.39, 2765.67, 5531.32]
+ @TETO = 4663.75
 end
+# CLASSE GERENCIAR EMPREGADOS
+class GerenciarEmpregados
+ @listaEmpregado = []
+
+ def adicionarEmpregado(e = Empregado)
+  @listaEmpregado.push(e)
+ end
+ def removerEmpregado(e = Empregado)
+  @listaEmpregado.delete(e)
+ end
+end
+# -----------------------------------------------------------------#
+emp = Empregado.new()
+puts "CODIGO : "
+emp.codigo = gets.chomp.to_i
+puts "NOME   : "
+emp.nome = gets.chomp
+puts "SETOR  : "
+emp.setor = gets.chomp
+puts "SALARIO: "
+emp.salarioBruto = gets.chomp.to_f
+
+
+
+=begin
+ge = GerenciarEmpregados.new
+ge.adicionarEmpregado(emp)
+ge.listaEmpregado.each do |item|
+    puts item
+end
+=end
